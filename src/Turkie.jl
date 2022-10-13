02a92d47890ad9e3ff87e3449fd89654bd83b660
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-10-13 17:16:30 +0200

    import Left

--- src/Turkie.jl
+++ src/Turkie.jl
@@ -1,6 +1,6 @@
 module Turkie
 
-using Makie: Makie, Figure, Axis, Point2f, Label
+using Makie: Makie, Figure, Axis, Point2f, Label, Left
 using Makie: barplot!, lines!, scatter! # Plotting tools
 using Makie: Observable, lift, on # Observable tools
 using Makie: recordframe! # Recording tools
