d99e930deefcf242050f3fe2eb71578b0cdb7df4
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-10-13 16:57:55 +0200

    Missing import

--- src/Turkie.jl
+++ src/Turkie.jl
@@ -1,6 +1,6 @@
 module Turkie
 
-using Makie: Makie, Figure, Axis, Point2f
+using Makie: Makie, Figure, Axis, Point2f, Label
 using Makie: barplot!, lines!, scatter! # Plotting tools
 using Makie: Observable, lift, on # Observable tools
 using Makie: recordframe! # Recording tools
